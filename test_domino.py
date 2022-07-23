2aec1b754d80163dd7c11542db5dd14f598f92d6
Adrian Charkiewicz <riraasaa@gmail.com>
2022-07-24 00:29:10 +0200

    more tests

--- test_domino.py
+++ test_domino.py
@@ -23,3 +23,35 @@ class Test(TestCase):
     def test_example_input(self):
         domino = "||//||\\||/\\|"
         assert iteration(domino) == "||///\\\\||/\\|"
+
+    def test_leaning(self):
+        domino = "/\\"
+        assert iteration(domino) == domino
+
+    def test_leaning2(self):
+        domino = "///\\\\\\"
+        assert iteration(domino) == domino
+
+    def test_middle(self):
+        # middle block should not fall to either side
+        domino = "/|\\"
+        assert iteration(domino) == domino
+
+    def test_middle2(self):
+        # middle block should not fall to either side, even when there is more blocks pushing it
+        domino = "/////////|\\"
+        assert iteration(domino) == domino
+
+    def test_two_iterations_right(self):
+        domino = "/||"
+        assert iteration(iteration(domino)) == "///"
+
+    def test_two_iterations_left(self):
+        domino = "||\\"
+        assert iteration(iteration(domino)) == "\\\\\\"
+
+    def test_ten_iterations(self):
+        domino = "/||||||||||||||||||||||||||||"
+        for i in range(10):
+            domino = iteration(domino)
+        assert domino == "///////////||||||||||||||||||"
