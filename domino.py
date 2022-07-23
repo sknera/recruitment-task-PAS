8ee93611aa7e6cdc64c0b67375435212fe801ed0
Adrian Charkiewicz <riraasaa@gmail.com>
2022-07-23 23:53:40 +0200

    added .py file, local iteration is no longer shadowed by result of  get() functions

---
+++ domino.py
@@ -0,0 +1,56 @@
+def get_first_right(start, changes_right):
+    for i in range(start, len(domino)):
+        if domino[i] == '/':
+            pass
+        elif domino[i] == '\\':
+            break
+        elif domino[i] == '|':
+            changes_right.append(i)
+            break
+    return i
+
+
+def get_first_left(start, changes_left):
+    bckw_domino = domino[::-1]
+    for i in range(start, len(domino)):
+        if bckw_domino[i] == '\\':
+            pass
+        elif bckw_domino[i] == '/':
+            break
+        elif bckw_domino[i] == '|':
+            changes_left.append(i)
+            break
+    return i
+
+
+def iteration(domino):
+    changes_right = list()
+    changes_left = list()
+    bckw_domino = domino[::-1]
+
+    for i in range(len(domino)):
+        if domino[i] == '/':
+            get_first_right(i, changes_right)
+
+    for i in range(len(domino)):
+        if bckw_domino[i] == '\\':
+            get_first_left(i, changes_left)
+    changes_left = [len_domino - x for x in changes_left]
+    intersection = list(set(changes_left) & set(changes_right))
+    changes_left = [x for x in changes_left if x not in intersection]
+    changes_right = [x for x in changes_right if x not in intersection]
+
+    list_domino = list(domino)
+    for i in changes_left:
+        list_domino[i] = "\\"
+    for i in changes_right:
+        list_domino[i] = "/"
+
+    print(changes_left)
+    print(changes_right)
+    return "".join(list_domino)
+
+
+domino = "||//||\\||/\\|"
+len_domino = len(domino) - 1
+print(iteration(domino))
