875725b6517a5a05bf4acbc5a507e48f23e9a68c
Adrian Charkiewicz <riraasaa@gmail.com>
2022-07-24 19:22:24 +0200

    functions removed from domino.py, set flags to check if domino block should fall

--- domino.py
+++ domino.py
@@ -1,4 +1,4 @@
-def get_first_right(domino, start, changes_right):
+"""def get_first_right(domino, start, changes_right):
     for i in range(start, len(domino)):
         if domino[i] == '/':
             pass
@@ -19,25 +19,45 @@ def get_first_left(domino, start, changes_left):
         elif bckw_domino[i] == '|':
             changes_left.append(i)
             break
+"""
 
 
 def iteration(domino):
-    len_domino = len(domino) - 1
+    len_domino = len(domino)
     changes_right = list()
     changes_left = list()
     bckw_domino = domino[::-1]
 
-    for i in range(len(domino)):
-        if domino[i] == '/':
-            get_first_right(domino, i, changes_right)
+    flagRight = False
+    for i in range(len_domino):
+        if flagRight and domino[i] == '\\':
+            flagRight = False
+        elif flagRight and domino[i] == '|':
+            changes_right.append(i)
+            flagRight = False
+            break
+        elif domino[i] == '/':
+            flagRight = True
 
-    for i in range(len(domino)):
-        if bckw_domino[i] == '\\':
-            get_first_left(domino, i, changes_left)
-    changes_left = [len_domino - x for x in changes_left]
+    flagLeft = False
+    for i in range(len_domino):
+        if flagLeft and bckw_domino[i] == '/':
+            flagLeft = False
+        elif flagLeft and bckw_domino[i] == '|':
+            changes_left.append(i)
+            flagLeft = False
+            break
+        elif bckw_domino[i] == '\\':
+            flagLeft = True
+
+    # found in backward domino, now set indexes that will be referenced in original
+    changes_left = [len_domino - x - 1 for x in changes_left]
     intersection = list(set(changes_left) & set(changes_right))
     changes_left = [x for x in changes_left if x not in intersection]
     changes_right = [x for x in changes_right if x not in intersection]
+
+    print(changes_left)
+    print(changes_right)
 
     list_domino = list(domino)
     for i in changes_left:
@@ -46,4 +66,3 @@ def iteration(domino):
         list_domino[i] = "/"
 
     return "".join(list_domino)
-
